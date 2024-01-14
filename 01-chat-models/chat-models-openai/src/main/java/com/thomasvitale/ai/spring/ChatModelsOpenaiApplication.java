4366616c09ea0949fdedd7921306257ead80152b
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-01-14 14:41:07 +0100

    Add chat-models-openai

---
+++ 01-chat-models/chat-models-openai/src/main/java/com/thomasvitale/ai/spring/ChatModelsOpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.thomasvitale.ai.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatModelsOpenaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatModelsOpenaiApplication.class, args);
+    }
+
+}
