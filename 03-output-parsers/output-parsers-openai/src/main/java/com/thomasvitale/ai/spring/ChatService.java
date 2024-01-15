69a0e3a7188d6579dea5a9246e1cfab51c4b9a30
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-01-15 21:46:41 +0100

    Add output-parsers sample

---
+++ 03-output-parsers/output-parsers-openai/src/main/java/com/thomasvitale/ai/spring/ChatService.java
@@ -0,0 +1,68 @@
+package com.thomasvitale.ai.spring;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.parser.BeanOutputParser;
+import org.springframework.ai.parser.ListOutputParser;
+import org.springframework.ai.parser.MapOutputParser;
+import org.springframework.ai.prompt.PromptTemplate;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+class ChatService {
+
+    private final ChatClient chatClient;
+
+    ChatService(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    ArtistInfo chatWithBeanOutput(MusicQuestion question) {
+        var outputParser = new BeanOutputParser<>(ArtistInfo.class);
+
+        var userPromptTemplate = new PromptTemplate("""
+                Tell me name and band of one musician famous for playing in a {genre} band.
+                Consider only the musicians that play the {instrument} in that band.
+                {format}
+                """);
+        Map<String,Object> model = Map.of("instrument", question.instrument(), "genre", question.genre(), "format", outputParser.getFormat());
+        var prompt = userPromptTemplate.create(model);
+
+        var chatResponse = chatClient.generate(prompt);
+        return outputParser.parse(chatResponse.getGeneration().getContent());
+    }
+
+    Map<String,Object> chatWithMapOutput(MusicQuestion question) {
+        var outputParser = new MapOutputParser();
+
+        var userPromptTemplate = new PromptTemplate("""
+                Tell me name and band of one musician famous for playing in a {genre} band.
+                Consider only the musicians that play the {instrument} in that band.
+                {format}
+                """);
+        Map<String,Object> model = Map.of("instrument", question.instrument(), "genre", question.genre(), "format", outputParser.getFormat());
+        var prompt = userPromptTemplate.create(model);
+
+        var chatResponse = chatClient.generate(prompt);
+        return outputParser.parse(chatResponse.getGeneration().getContent());
+    }
+
+    List<String> chatWithListOutput(MusicQuestion question) {
+        var outputParser = new ListOutputParser(new DefaultConversionService());
+
+        var userPromptTemplate = new PromptTemplate("""
+                Tell me names of three musicians famous for playing in a {genre} band.
+                Consider only the musicians that play the {instrument} in that band.
+                {format}
+                """);
+        Map<String,Object> model = Map.of("instrument", question.instrument(), "genre", question.genre(), "format", outputParser.getFormat());
+        var prompt = userPromptTemplate.create(model);
+
+        var chatResponse = chatClient.generate(prompt);
+        return outputParser.parse(chatResponse.getGeneration().getContent());
+    }
+
+}
---
+++ 03-output-parsers/output-parsers-openai/src/main/java/com/thomasvitale/ai/spring/OutputParsersBeanOpenAiApplication.java
@@ -0,0 +1,13 @@
+package com.thomasvitale.ai.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutputParsersBeanOpenAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutputParsersBeanOpenAiApplication.class, args);
+    }
+
+}
