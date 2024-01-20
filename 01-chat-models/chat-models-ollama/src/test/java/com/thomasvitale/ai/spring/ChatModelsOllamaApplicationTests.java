2d825eea1beef87c0467f01eea0e11bc921a41be
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-01-20 21:36:37 +0100

    Improve chat-models-ollama example

--- 01-chat-models/chat-models-ollama/src/test/java/com/thomasvitale/ai/spring/ChatModelsOllamaApplicationTests.java
+++ 01-chat-models/chat-models-ollama/src/test/java/com/thomasvitale/ai/spring/ChatModelsOllamaApplicationTests.java
@@ -1,13 +1,32 @@
 package com.thomasvitale.ai.spring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestChatModelsOllamaApplication.class)
+@AutoConfigureWebTestClient(timeout = "30s")
 class ChatModelsOllamaApplicationTests {
 
+    @Autowired
+    WebTestClient webTestClient;
+
     @Test
-    void contextLoads() {
+    void chat() {
+        webTestClient
+                .get()
+                .uri("/ai/chat")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).value(response -> {
+                   assertThat(response).contains("You shall not pass");
+                });
     }
 
 }
--- 01-chat-models/chat-models-ollama/src/test/java/com/thomasvitale/ai/spring/TestChatModelsOllamaApplication.java
+++ 01-chat-models/chat-models-ollama/src/test/java/com/thomasvitale/ai/spring/TestChatModelsOllamaApplication.java
@@ -2,9 +2,22 @@ package com.thomasvitale.ai.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.GenericContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestChatModelsOllamaApplication {
+
+    @Bean
+    GenericContainer<?> ollama(DynamicPropertyRegistry properties) {
+        var ollama = new GenericContainer<>("ghcr.io/thomasvitale/ollama-llama2")
+                .withExposedPorts(11434);
+        properties.add("spring.ai.ollama.base-url",
+                () -> "http://%s:%s".formatted(ollama.getHost(), ollama.getMappedPort(11434)));
+        properties.add("spring.ai.ollama.chat.model", () -> "llama2");
+        return ollama;
+    }
 
     public static void main(String[] args) {
         SpringApplication.from(ChatModelsOllamaApplication::main).with(TestChatModelsOllamaApplication.class).run(args);
