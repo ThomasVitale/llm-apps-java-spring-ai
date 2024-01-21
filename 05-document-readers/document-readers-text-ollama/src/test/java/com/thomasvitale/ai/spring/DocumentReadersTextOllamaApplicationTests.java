3aa94a2f07aa3fe5971c4e08bf6eed85745a213f
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-01-21 22:45:02 +0100

    Update document readers tests

--- 05-document-readers/document-readers-text-ollama/src/test/java/com/thomasvitale/ai/spring/DocumentReadersTextOllamaApplicationTests.java
+++ 05-document-readers/document-readers-text-ollama/src/test/java/com/thomasvitale/ai/spring/DocumentReadersTextOllamaApplicationTests.java
@@ -2,8 +2,10 @@ package com.thomasvitale.ai.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestDocumentReadersTextOllamaApplication.class)
 class DocumentReadersTextOllamaApplicationTests {
 
     @Test
