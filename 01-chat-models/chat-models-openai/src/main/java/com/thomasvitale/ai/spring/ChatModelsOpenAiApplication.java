7e5f8b99669bf4f07f1b2234b0579b2f3b2168d0
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-04-28 23:22:29 +0200

    Fix case sensitive issue
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- 01-chat-models/chat-models-openai/src/main/java/com/thomasvitale/ai/spring/ChatModelsOpenAiApplication.java
+++ 01-chat-models/chat-models-openai/src/main/java/com/thomasvitale/ai/spring/ChatModelsOpenAiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatModelsOpenAApplication {
+public class ChatModelsOpenAiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatModelsOpenAApplication.class, args);
+        SpringApplication.run(ChatModelsOpenAiApplication.class, args);
     }
 
 }
--- 01-chat-models/chat-models-openai/src/test/java/com/thomasvitale/ai/spring/TestChatModelsOpenaiApplication.java
+++ 01-chat-models/chat-models-openai/src/test/java/com/thomasvitale/ai/spring/TestChatModelsOpenaiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 public class TestChatModelsOpenAiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(ChatModelsOpenAApplication::main).with(TestChatModelsOpenAiApplication.class).run(args);
+        SpringApplication.from(ChatModelsOpenAiApplication::main).with(TestChatModelsOpenAiApplication.class).run(args);
     }
 
 }
