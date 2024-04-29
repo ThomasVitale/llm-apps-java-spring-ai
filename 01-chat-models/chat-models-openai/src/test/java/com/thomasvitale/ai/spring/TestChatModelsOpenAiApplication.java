1e68816b35a6fbded547deafa3625094bec06bc8
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-04-29 21:09:18 +0200

    Refactor
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- 01-chat-models/chat-models-openai/src/test/java/com/thomasvitale/ai/spring/TestChatModelsOpenAiApplication.java
+++ 01-chat-models/chat-models-openai/src/test/java/com/thomasvitale/ai/spring/TestChatModelsOpenAiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 
 @TestConfiguration(proxyBeanMethods = false)
-public class TestChatModelsOpenAApplication {
+public class TestChatModelsOpenAiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(ChatModelsOpenAiApplication::main).with(TestChatModelsOpenAApplication.class).run(args);
+        SpringApplication.from(ChatModelsOpenAiApplication::main).with(TestChatModelsOpenAiApplication.class).run(args);
     }
 
 }
