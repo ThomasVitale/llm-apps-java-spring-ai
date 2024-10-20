c024bd6c86076f84c8fdeb9bc6ca491cf1e8247c
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-10-20 18:15:17 +0200

    Update tests for use cases projects

--- 00-use-cases/text-classification/src/test/java/com/thomasvitale/ai/spring/TextClassificationApplicationTests.java
+++ 00-use-cases/text-classification/src/test/java/com/thomasvitale/ai/spring/TextClassificationApplicationTests.java
@@ -2,8 +2,10 @@ package com.thomasvitale.ai.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestcontainersConfiguration.class)
 class TextClassificationApplicationTests {
 
     @Test
